607b54ad39deb8563f4c5a5ad32a54d234ad1469
Alexander Korn <akorn@gustavus.edu>
2018-03-27 21:50:04 -0500

    update
    Added Buttons for showing our arrays and adding to each specific array depending on important

--- Group Chrome Extension/textBox.js
+++ Group Chrome Extension/textBox.js
@@ -1,6 +1,43 @@
-var email = document.getElementByID("emailText");
-var emailArray = [];
+var email;
+var emailReallyImportantArray = [];
+var emailImportantArray= [];
+var emailNonImportantArray = [];
 
-function addEmail(email){
-  emailArray.push(email);
+function addImportantEmail(){
+  email = document.getElementById("emailText").value;
+  emailReallyImportantArray.push(email);
+  //document.getElementById("emailText").value = "";
+}
+
+function addImportantEmail(){
+  email = document.getElementById("emailText").value;
+  emailImportantArray.push(email);
+  //document.getElementById("emailText").value = "";
+}
+
+function addNotImportantEmail(){
+  email = document.getElementById("emailText").value;
+  emailNonImportantArray.push(email);
+  //document.getElementById("emailText").value = "";
+}
+
+function printReallyImportantArray(emailReallyImportantArray){
+  for (var i = 0; i<emailReallyImportantArray.length; i++){
+    //document.writeln(emailReallyImportantArray[i] + "/n");
+    console.log(emailReallyImportantArray[i]);
+  }
+}
+
+function printImportantArray(emailImportantArray){
+  for (var i = 0; i<emailImportantArray.length; i++){
+    //document.writeln(emailImportantArray[i] + "/n");
+    console.log(emailImportantArray[i]);
+  }
+}
+
+function printNotImportantArray(emailNonImportantArray){
+  for (var i = 0; i<emailNonImportantArray.length; i++){
+    //document.writeln(emailNonImportantArray[i] + "/n");
+    console.log(emailNotImportantArray[i]);
+  }
 }
