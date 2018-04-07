bdd12ee3221031033e2518dcbf1d8d9abe44d4db
Alexander Korn <akorn@gustavus.edu>
2018-04-06 23:06:48 -0500

    stopwatch added

--- Group Chrome Extension/backgroud.js
+++ Group Chrome Extension/backgroud.js
@@ -1,22 +1,42 @@
+//var _StopWatch = new StopWatch();
+var starTime = null;
+var endTime = null;
+var totalTime = null;
+//var totaalTIme = null;
+
 function notify(email){
   //calls notify for really important if it exists in really important array
-  if(emailReallyImportantArray.indexOf(email) != -1){
+  if(emailReallyImportantArray.includes(email) == true){
     notifyReallyImportant(); //doesnt exist yet
   }
   //calls notify for important if it exsits in important array
-  else if(emailReallyImportantArray.indexOf(email) != -1){
-    notifyImportant(); //doesnt exist yet
+  else if(emailReallyImportantArray.includes(email) == true){
+    notifyImportant();//doesnt exist yet
   }
   //calls notify for not important if it exists in not important array
-  else if(emailReallyImportantArray.indexOf(email) != -1){
+  else if(emailReallyImportantArray.includes(email) == true){
     notifyNonImportant();//doesnt exist yet
   }
   else{
     return;
   }
+  //Date.now();
 }
 
-function timer(){
-
+function getTime(){
+  var day = new Date();
+  return day.getTime();
 }
-  
+
+function getEndTime(){
+  endTime = getTime();
+}
+
+function getStartTime(){
+  startTime = getTime();
+}
+
+function calculateTime(){
+  totalTime = startTime - endTime
+  return totalTime;
+}
