0ec4335649578739d22a284043df5b26259f98e3
Alexander Korn <akorn@gustavus.edu>
2018-05-08 14:38:09 -0500

    Local Storage

--- Group Chrome Extension/options.js
+++ Group Chrome Extension/options.js
@@ -8,49 +8,50 @@ var emailNonImportantArray = [];
 function addReallyImportantEmail(){
   email = document.getElementById("emailText").value;
   emailReallyImportantArray.push(email);
+  localStorage.setItem('reallyImportantStorage', JSON.stringify(emailReallyImportantArray));
   //document.getElementById("emailText").value = "";
 }
 
 function addImportantEmail(){
   email = document.getElementById("emailText").value;
   emailImportantArray.push(email);
+    localStorage.setItem('importantStorage', JSON.stringify(emailImportantArray));
   //document.getElementById("emailText").value = "";
 }
 
 function addNotImportantEmail(){
   email = document.getElementById("emailText").value;
   emailNonImportantArray.push(email);
+  localStorage.setItem('notImportantStorage', JSON.stringify(emailNonImportantArray));
   //document.getElementById("emailText").value = "";
 }
 
-function printReallyImportantArray(emailReallyImportantArray){
-  for (var i = 0; i<emailReallyImportantArray.length; i++){
-    //document.writeln(emailReallyImportantArray[i] + "/n");
-    console.log(emailReallyImportantArray[i]);
-  }
+function save(whichArray){
+  localStorage.setItem('theArray',JSON.stringify(whichArray));
+
 }
 
-function printImportantArray(emailImportantArray){
-  for (var i = 0; i<emailImportantArray.length; i++){
-    //document.writeln(emailImportantArray[i] + "/n");
-    console.log(emailImportantArray[i]);
-  }
-}
 
-function printNotImportantArray(emailNonImportantArray){
-  for (var i = 0; i<emailNonImportantArray.length; i++){
-    //document.writeln(emailNonImportantArray[i] + "/n");
-    console.log(emailNotImportantArray[i]);
-  }
-}
+$("#reallyImportant").click(function(){
+  addReallyImportantEmail();
+});
 
-function clearEmailTextField(){
-  document.getElementById("emailText").value = "";
-}
+$("#Important").click(function(){
+  addImportantEmail();
+});
 
-//document.getElementById("reallyImportant").addEventListener("click", addReallyImportantEmail());
-//document.getElementById("Important").addEventListener("click", addImportantEmail());
-//document.getElementById("notImportant").addEventListener("click", addNotImportantEmail());
-//document.getElementById("reallyImportantPrintButton").addEventListener("click",printReallyImportantArray(emailReallyImportantArray));
-//document.getElementById("importantPrintButton").addEventListener("click", printImportantArray(emailImportantArray));
-//document.getElementById("notImportantPrintButton").addEventListener("click", printNotImportantArray(emailNonImportantArray));
+$("#notImportant").click(function(){
+  addNotImportantEmail();
+});
+
+$("#reallyImportantPrintButton").click(function(){
+  console.log(emailReallyImportantArray);
+});
+
+$("#importantPrintButton").click(function(){
+  console.log(emailImportantArray);
+});
+
+$("#notImportantPrintButton").click(function(){
+  console.log(emailNonImportantArray);
+});
